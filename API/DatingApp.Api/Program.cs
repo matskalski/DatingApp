bfd2dbc2662623b227b15b79cca150d9bbc91931
Mateusz Skalski <mat.skalski@wp.pl>
2025-08-27 21:27:23 +0200

    zmiana

--- API/DatingApp.Api/Program.cs
+++ API/DatingApp.Api/Program.cs
@@ -1,8 +1,15 @@
+using DatingApp.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
