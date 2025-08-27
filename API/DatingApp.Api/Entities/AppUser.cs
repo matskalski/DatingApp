c37ead2b07810bd6516fdb4616c9c76f1b6819b6
Mateusz Skalski <mat.skalski@wp.pl>
2025-08-27 21:17:32 +0200

    initial commit

---
+++ API/DatingApp.Api/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace DatingApp.Api.Entities
+{
+    public class AppUser
+    {
+        public required string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string DisplayName { get; set; }
+        public required string Email { get; set; }
+    }
+}
