e376686f487c04e462e9a283eb45b3fe3586fc9f
Mateusz Skalski <Mateusz.Skalski@rossmann.com.pl>
2025-09-01 15:58:49 +0200

    zmiana

--- UI/DatingApp/src/app/app.ts
+++ UI/DatingApp/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MembersService } from './services/members-service';
 
 @Component({
   selector: 'da-root',
@@ -7,6 +8,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('DatingApp');
+export class App implements OnInit {
+  membersService : MembersService = inject(MembersService);
+
+  ngOnInit(): void {
+    this.membersService.getMembers()
+      .subscribe()
+  }
+
 }
---
+++ UI/DatingApp/src/app/services/members-service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Member } from '../models/member';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  httpClient: HttpClient = inject(HttpClient)
+
+  getMembers() : Observable<Member[]> {
+    return this.httpClient.get<Member[]>("https://localhost:7144/api/members")
+  }
+}
