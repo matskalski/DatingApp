ca2e0542e4641d9626f1d045a1ad9ecc6727fbf3
Mateusz Skalski <Mateusz.Skalski@rossmann.com.pl>
2025-09-04 15:54:57 +0200

    zmiana

--- UI/DatingApp/src/app/components/nav/nav.ts
+++ UI/DatingApp/src/app/components/nav/nav.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'da-nav',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
   protected creds: any = {}
+
+  //private fb = inject(FormBuilder);
+  constructor(private fb: FormBuilder) { }
+
+  form: FormGroup = this.fb.group({
+    email: ['', []],
+    password: ['', []]
+  })
+
+  login(){
+    console.log('');
+  }
 }
