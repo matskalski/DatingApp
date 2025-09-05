142188b3b3319e169cb9f4eddd6da5b0abcf45be
Mateusz Skalski <Mateusz.Skalski@rossmann.com.pl>
2025-09-05 15:48:29 +0200

    zmiana

---
+++ UI/DatingApp/src/app/components/home/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'da-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
